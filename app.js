44de422569421357915b033d5346f1b83529d214
nathaliefc1 <nathaliefc89@gmail.com>
2023-03-20 15:27:14 +0100

    solution finich+ server

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const puzzleBoard = document.querySelector('#puzzle')
 const solveButton = document.querySelector('#solve-button')
 const solutionDisplay = document.querySelector('#solution')
 const squares = 81
-const submission = []
+let submission = []
 
 for (let i=0; i< squares; i++) {
     const inputElement = document.createElement('input')
     inputElement.setAttribute('type', 'number')
-    inputElement.setAttribute('min', '1')
-    inputElement.setAttribute('max', '9')
+    inputElement.setAttribute('min', 1)
+    inputElement.setAttribute('max', 9)
     if (
         ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
         ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
@@ -27,15 +27,15 @@ const joinValues = () => {
         if(input.value) {
             submission.push(input.value)
         }else {
-            submission.push('0')
+            submission.push('.')
         }
     })
-    console.log(submission)
+    console.log('submission', submission)
 }
 
-const populateValues = (isSolvable, solution) => {
+const populateValues = (solvable, solution) => {
     const inputs = document.querySelectorAll('input')
-    if (isSolvable && solution){
+    if (solvable && solution){
         inputs.forEach((input, i)=> {
             input.value = solution[i]
         })
@@ -47,27 +47,23 @@ const populateValues = (isSolvable, solution) => {
 
 const solve = () => {
     joinValues()
-    const data = submission.join('')
-    console.log('data', data.length)
-    const options = {
+    const data = {numbers: submission.join('')}
+    console.log('data', data)
+    fetch('http://localhost:8000/solve', {
         method: 'POST',
-        url: 'https://solve-sudoku.p.rapidapi.com/',
         headers: {
-          'content-type': 'application/json',
-          'X-RapidAPI-Key': Process.env.RAPID_API_KEY,
-          'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com'
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
         },
-        data: {
-            puzzle:data
-        }
-      };
-      
-      axios.request(options).then( (response) => {
-          console.log(response.data)
-          populateValues(response.data.solvable, response.data.solution)
-      }).catch((error) => {
-          console.error(error)
-      });
+        body: JSON.stringify(data)
+    }). then(response => response.json())
+    .then(data => {
+        populateValues(data.solvable, data.solution)
+        submission = []
+    })
+    .catch((error) => {
+        console.error('Error:', error)
+    })
 }
 
 solveButton.addEventListener('click', solve)
